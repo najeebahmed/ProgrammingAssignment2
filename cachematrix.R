a2b4005123bb9abc991cf7409d035fc512656149
Najeeb Sajid <nahmed3@asu.edu>
2017-05-10 18:05:23 -0700

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Decreases time to get inverse of matrix
+##  repeatedly by caching inverse
 
-## Write a short comment describing this function
+## Creates template list for cachable matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
